7021ad831611c793725e1eccf861344c0caebb60
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-11-16 12:00:03 +0530

    connected mongodb and created userschema

---
+++ backend/models/userSchema.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true 
+        },
+        email: {
+            type: String, 
+            required: true, 
+            unique: true
+        },
+        password: {
+            type: String, 
+            required: true
+        },
+        cart: [
+            {
+                item: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Product",
+                    required: true,
+                },
+                quantity: {
+                    type: Number, 
+                    default: 1
+                },
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/connectDB.js';
+import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
+
+dotenv.config();
 
 const port = 5000;
 const app = express();
+
+// Connecting MongoDB
+connectDB();
+
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+// Middleware to parse URL-encoded data
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('tiny')); // Logging requests
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
