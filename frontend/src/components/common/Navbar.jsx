7022bf4b78e179a3788fb4d9a7512c5921bd8b6b
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-11-22 13:41:42 +0530

    upated navbar ui

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -6,38 +6,31 @@ import PurpleCircleButton from "../reusable/PurpleCircleButton";
 
 const Navbar = () => {
     return (
-        <div className='flex items-center w-full h-16 bg-purple-100'>
-            <div className='w-1/2 pl-20 flex gap-5 text-sm font-semibold'>
-                <div className='rounded-full'>
-                    <div>
-
+        <div className="flex items-center w-full h-16 bg-purple-100 px-4 md:px-8 lg:px-16">
+            {/* Left Section */}
+            <div className="w-1/2 flex gap-3 md:gap-5 text-sm font-semibold flex-wrap md:flex-nowrap">
+                <div className="flex items-center gap-2">
+                    <div className="w-8 h-8 bg-purple-300 rounded-full flex items-center justify-center">
+                        {/* Optional Icon/Logo */}
                     </div>
-                    <h3>
-                        All Categories
-                    </h3>
+                    <h3 className="hidden sm:block">All Categories</h3>
                 </div>
-                <button>
-                    Deals 
-                </button>
-                <button>
-                    Shop  
-                </button>
-                <button>
-                    Our contacts 
-                </button>
-                <button>
-                    Stores 
-                </button>
+                <button className="hover:text-purple-700">Deals</button>
+                <button className="hover:text-purple-700">Shop</button>
+                <button className="hover:text-purple-700 hidden md:block">Our Contacts</button>
+                <button className="hover:text-purple-700 hidden lg:block">Stores</button>
             </div>
-            <div className='w-1/2 pr-20 flex justify-end gap-4'>
-            <WhiteCircleButton value={<CiUser />} />
-            <WhiteCircleButton value={<CiHeart />} />
-                <Link to='/cart'>
+
+            {/* Right Section */}
+            <div className="w-1/2 flex justify-end gap-2 sm:gap-4">
+                <WhiteCircleButton value={<CiUser />} />
+                <WhiteCircleButton value={<CiHeart />} />
+                <Link to="/cart">
                     <PurpleCircleButton value={<GrCart />} />
                 </Link>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
